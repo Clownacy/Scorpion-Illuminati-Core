f09c66a0345d015f946c7f410fc4a1111a06df75
Scorpion Illuminati <webmaster@scorpionilluminati.net>
2016-08-07 02:43:47 -0400

    Initial Commit

---
+++ src/assets/tiles/titlescreentiles.asm
@@ -0,0 +1,148 @@
+;==============================================================
+;   Scorpion Illuminati
+;==============================================================
+;   SEGA Genesis (c) SegaDev 2014
+;==============================================================
+
+TitleScreenTiles:
+      dc.l	$00000000                                                          ; 
+      dc.l	$00000000                                                          ;
+      dc.l	$00000000                                                          ;
+      dc.l	$00000000                                                          ;
+      dc.l	$00000000                                                          ;
+      dc.l	$00000000                                                          ;
+      dc.l	$00000000                                                          ;
+      dc.l	$00000000                                                          ; 
+
+      dc.l  $44444444                                                          ; XXXXXXXX
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $44444444                                                          ; XXXXXXXX
+
+      dc.l  $44444444                                                          ; XXXXXXXX
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $44444444                                                          ; XXXXXXXX
+
+      dc.l  $44444444                                                          ; XXXXXXXX
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $44444444                                                          ; XXXXXXXX
+
+      dc.l  $44444444                                                          ; XXXXXXXX
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $44444444                                                          ; XXXXXXXX
+
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+      dc.l  $43333334                                                          ; X      X
+
+      dc.l  $44444444                                                          ; XXXXXXXX
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X
+
+      dc.l  $43333333                                                          ; X
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $44444444                                                          ; XXXXXXXX
+
+      dc.l  $44444444                                                          ; XXXXXXXX
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $44444444                                                          ; XXXXXXXX
+
+      dc.l  $44444444                                                          ; XXXXXXXX 
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $33333333                                                          ;         
+      dc.l  $44444444                                                          ; XXXXXXXX
+
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+      dc.l  $43333333                                                          ; X       
+
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+      dc.l  $33333334                                                          ;        X
+
+TitleScreenTilesEnd                                                            ; Tiles end address
+TitleScreenTilesSizeB: equ (TitleScreenTilesEnd-TitleScreenTiles)              ; Tiles size in bytes
+TitleScreenTilesSizeW: equ (TitleScreenTilesSizeB/SizeWord)                    ; Tiles size in words
+TitleScreenTilesSizeL: equ (TitleScreenTilesSizeB/SizeLong)                    ; Tiles size in longs
+TitleScreenTilesSizeT: equ (TitleScreenTilesSizeB/SizeTile)                    ; Tiles size in tiles
+TitleScreenTilesTileID: equ (TitleScreenTilesVRAM/SizeTile)                    ; ID of first tile
