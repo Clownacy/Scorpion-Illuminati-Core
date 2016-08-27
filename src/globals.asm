2e8aae39fcea55b35277a4c2a987dff7c6208c5d
Scorpion Illuminati <webmaster@scorpionilluminati.net>
2016-08-26 22:43:19 -0400

    Fixed typo with game mode game state constant.

--- src/globals.asm
+++ src/globals.asm
@@ -17,7 +17,7 @@ screen_border_y      equ 0x0010
 game_state_title_screen_initalize equ 0x0000
 game_state_title_screen           equ 0x0001
 game_state_game_mode_initalize    equ 0x0002
-game_state_game_mode              equ 0x0004
+game_state_game_mode              equ 0x0003
 
 ; note collision bounds
 note_bounds_top      equ 0x0000
